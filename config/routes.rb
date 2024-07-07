c93936bb6ef21a2bcba9f3564cbf86ac6cb4c0b0
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-07-07 13:14:10 +0200

    index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'medical_cares', to: 'medical_cares#index'
+  get 'medical_cares/:id', to: 'medical_cares#show'
 end
