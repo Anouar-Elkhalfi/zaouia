e90b44b479737b67ce34b235cdfb4b5acc2b712b
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-07-07 14:04:43 +0200

    show

--- app/controllers/medical_cares_controller.rb
+++ app/controllers/medical_cares_controller.rb
@@ -1,5 +1,12 @@
 class MedicalCaresController < ApplicationController
+  # Action index pour afficher toutes les instances de MedicalCare
   def index
+    # @medical_cares est une collection (toutes les instances de MedicalCare)
     @medical_cares = MedicalCare.all
+  end
+ # Action show pour afficher une seule instance de MedicalCare
+  def show
+    # @medical_care est une seule instance de MedicalCare, trouvée par son ID
+    @medical_care = MedicalCare.find(params[:id])
   end
 end
