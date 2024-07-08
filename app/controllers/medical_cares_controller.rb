1a16c030c1af18ccdb1d5bb3a8aaeaff8178ab27
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-07-08 14:41:09 +0200

    CRUD

--- app/controllers/medical_cares_controller.rb
+++ app/controllers/medical_cares_controller.rb
@@ -1,4 +1,5 @@
 class MedicalCaresController < ApplicationController
+  before_action :set_medical_care, only: [:show, :edit, :update, :destroy]
   # Action index pour afficher toutes les instances de MedicalCare
   def index
     # @medical_cares est une collection (toutes les instances de MedicalCare)
@@ -7,7 +8,6 @@ class MedicalCaresController < ApplicationController
  # Action show pour afficher une seule instance de MedicalCare
   def show
     # @medical_care est une seule instance de MedicalCare, trouvée par son ID
-    @medical_care = MedicalCare.find(params[:id])
   end
 
   def new
@@ -17,11 +17,26 @@ class MedicalCaresController < ApplicationController
   def create
     @medical_care = MedicalCare.new(medical_care_params)
     @medical_care.save
-    redirect_to medical_care_path(@medical_care)
+    redirect_to medical_care_path(@medical_care), notice: 'Medical Care was successfully created.'
   end
+
+  def edit
+  end
+  def update
+    @medical_care.update(medical_care_params)
+    redirect_to medical_care_path(@medical_care), notice: 'Medical Care was successfully updated.'
+  end
+  def destroy
+    @medical_care.destroy
+    redirect_to medical_cares_path, status: :see_other, notice: 'Medical Care was successfully destroyed.'
+  end
+
   private
 
   def medical_care_params
-    params.require(:medical_care).permit(:name, :adress, :rating, :phone)
+    params.require(:medical_care).permit(:name,:rating, :address, :phone)
+  end
+  def set_medical_care
+    @medical_care = MedicalCare.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get 'medical_cares', to: 'medical_cares#index'
-  get 'medical_cares/new', to: 'medical_cares#new'
-  get 'medical_cares/:id', to: 'medical_cares#show', as: 'medical_care'
-  post 'medical_cares', to: 'medical_cares#create'
+  # get 'medical_cares', to: 'medical_cares#index', as: 'medical_cares'
+  # get 'medical_cares/new', to: 'medical_cares#new', as: 'new_medical_care'
+  # get 'medical_cares/:id', to: 'medical_cares#show', as: 'medical_care'
+  # post 'medical_cares', to: 'medical_cares#create'
+  # get 'medical_cares/:id/edit', to: 'medical_cares#edit', as: 'edit_medical_care'
+  # patch 'medical_cares/:id', to: 'medical_cares#update'
+  # delete 'medical_cares/:id', to: 'medical_cares#destroy'
+  resources :medical_cares
 end
