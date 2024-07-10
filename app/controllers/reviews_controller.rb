3fca1f5dd6652d8dba1a7fdc2aaee5d582e0de8c
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-07-10 14:32:45 +0200

    Reviews-new/create & destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,28 @@
 class ReviewsController < ApplicationController
   def new
     # We need @medicalcare in our `simple_form_for`
-    @medicalcare = MedicalCare.find(params[:medical_care_id])
+    @medical_care = MedicalCare.find(params[:medical_care_id])
     @review = Review.new
   end
   def create
     #construire la review avec le content
     @review = Review.new(review_params)
-    raise
     # trouver le medical care
-    @medicalcare = MedicalCare.find(params[:medical_care_id])
+    @medical_care = MedicalCare.find(params[:medical_care_id])
     # associer la review avec le medical care
-
-
+    @review.medical_care = @medical_care
+    # save the review
+    if @review.save
+    redirect_to medical_care_path(@medical_care), notice: 'Review was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @review = Review.find(params[:id])
+    # @medical_care = @review.medical_care
+    @review.destroy
+    redirect_to medical_care_path(@review.medical_care), status: :see_other,notice: 'Review was successfully deleted.'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :medical_cares do
     resources :reviews, only: [:new, :create]
   end
+  resources :reviews, only: [:destroy]
   # get 'medical_cares', to: 'medical_cares#index', as: 'medical_cares'
   # get 'medical_cares/new', to: 'medical_cares#new', as: 'new_medical_care'
   # get 'medical_cares/:id', to: 'medical_cares#show', as: 'medical_care'
@@ -12,4 +13,5 @@ Rails.application.routes.draw do
   # get 'medical_cares/:id/edit', to: 'medical_cares#edit', as: 'edit_medical_care'
   # patch 'medical_cares/:id', to: 'medical_cares#update'
   # delete 'medical_cares/:id', to: 'medical_cares#destroy'
+  # <==>  resources :medical_cares
 end
