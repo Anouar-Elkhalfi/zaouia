4e05fab37d4d23dae8ba72b93684a08ae02f92cc
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-09-02 13:13:09 +0200

    open a project plato

--- app/controllers/admin/medical_cares_controller.rb
+++ app/controllers/admin/medical_cares_controller.rb
@@ -8,22 +8,39 @@ module Admin
 
     def create
       @medical_care = MedicalCare.new(medical_care_params)
+      @medical_care.user = current_user  # Assigne l'utilisateur courant (l'administrateur) au MedicalCare
+
+      puts @medical_care.inspect  # Affiche les détails de l'objet dans le terminal
+
       if @medical_care.save
-        redirect_to medical_care_path(@medical_care), notice: 'Medical care was successfully created.'
+        puts "Medical care saved successfully."  # Message de succès
+        redirect_to admin_medical_care_path(@medical_care), notice: 'Medical care was successfully created.'
       else
+        puts "Failed to save medical care."  # Message d'échec
+        puts @medical_care.errors.full_messages  # Affiche les erreurs de validation
         render :new, status: :unprocessable_entity
       end
     end
 
+
     def edit
+      puts "Params ID: #{params[:id]}"  # Ajoutez ce debug
       @medical_care = MedicalCare.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to medical_cares_path, alert: "Medical care not found."
     end
+
 
     def update
       @medical_care = MedicalCare.find(params[:id])
+      puts @medical_care.inspect  # Affiche les détails de l'objet dans le terminal
+
       if @medical_care.update(medical_care_params)
+        puts "Medical care updated successfully."  # Message de succès
         redirect_to medical_care_path(@medical_care), notice: 'Medical care was successfully updated.'
       else
+        puts "Failed to update medical care."  # Message d'échec
+        puts @medical_care.errors.full_messages  # Affiche les erreurs de validation
         render :edit, status: :unprocessable_entity
       end
     end
@@ -41,7 +58,7 @@ module Admin
     end
 
     def medical_care_params
-      params.require(:medical_care).permit(:name, :rating, :address, :phone)
+      params.require(:medical_care).permit(:name, :rating, :address, :phone, :user_id)
     end
   end
 end
