7b45e4271138a9c1bdfdfd71642623a5cefec862
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-10-10 13:59:22 +0200

    role

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,18 @@ class User < ApplicationRecord
   # Devise inclus si tu l'utilises
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  # Méthode pour vérifier le rôle 'admin'
+  # Un utilisateur peut avoir plusieurs MedicalCares
+  has_many :medical_cares, dependent: :destroy
+
+  # Méthodes de rôle
   def admin?
     role == 'admin'
   end
 
-  # Méthode pour vérifier le rôle 'doctor'
   def doctor?
     role == 'doctor'
   end
 
-  # Méthode pour vérifier le rôle 'patient'
   def patient?
     role == 'patient'
   end
